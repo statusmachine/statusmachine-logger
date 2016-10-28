483edbcfb493314dc5374542fc89ff3c5783ccda
Julien Desrosiers <juliend2@gmail.com>
2016-10-28 13:57:55 -0400

    ARYO

--- notifications/class-aal-notification-email.php
+++ notifications/class-aal-notification-email.php
@@ -19,7 +19,6 @@ class AAL_Notification_Email extends AAL_Notification_Base {
 	public function init() {
 		$this->options = array_merge( array(
 			'from_email'   => get_option( 'admin_email' ),
-//			'message_format' => __( "Hi there!\n\nA notification condition on [sitename] was matched. Here are the details:\n\n[action-details]\n\nSent by ARYO Activity Log", 'status-machine' )
 		), $this->get_handler_options() );
 	}
 	
@@ -62,7 +61,7 @@ class AAL_Notification_Email extends AAL_Notification_Base {
 	}
 	
 	public function settings_fields() {
-		$default_email_message = __( "Hi there!\n\nA notification condition on [sitename] was matched. Here are the details:\n\n[action-details]\n\nSent by ARYO Activity Log", 'status-machine' );
+		$default_email_message = __( "Hi there!\n\nA notification condition on [sitename] was matched. Here are the details:\n\n[action-details]\n\nSent by Status Machine", 'status-machine' );
 
 		$this->add_settings_field_helper( 'from_email', __( 'From Email', 'status-machine' ), array( 'AAL_Settings_Fields', 'text_field' ), __( 'The source Email address', 'status-machine' ) );
 		$this->add_settings_field_helper( 'to_email', __( 'To Email', 'status-machine' ), array( 'AAL_Settings_Fields', 'text_field' ), __( 'The Email address notifications will be sent to', 'status-machine' ) );
